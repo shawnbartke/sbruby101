6be52ef1271b43d67842456eb1e775694b039d5d
Shawn Bartke <cfsilversnake@gmail.com>
2022-06-24 18:10:35 +0000

    Rubocop changes

--- lesson2/l2.rb
+++ lesson2/l2.rb
@@ -23,7 +23,7 @@ prompt("Welcome to Calculator! Enter your name:")
 name = ''
 loop do
   name = Kernel.gets().chomp()
-  
+
   if name.empty?()
     prompt("Make sure to use a valid name.")
   else
@@ -33,32 +33,32 @@ end
 
 prompt("Hi #{name}.")
 
-loop do #main loop
+loop do # main loop
   number1 = ''
   loop do
-  prompt("What's the first number?")
-  number1 = Kernel.gets().chomp()
-  
+    prompt("What's the first number?")
+    number1 = Kernel.gets().chomp()
+
     if valid_number?(number1)
       break
     else
       prompt("Hmm... that doesn't look like a valid number")
     end
   end
-  
+
   number2 = ''
   loop do
-  prompt("What's the second number?")
-  number2 = Kernel.gets().chomp()
-  
+    prompt("What's the second number?")
+    number2 = Kernel.gets().chomp()
+
     if valid_number?(number2)
       break
     else
       prompt("Hmm... that doesn't look like a valid number")
-    end 
+    end
   end
-  
-  operator_prompt = <<-MSG #MSG can be anything just has to match(delimiters)
+
+  operator_prompt = <<-MSG # MSG can be anything just has to match(delimiters)
     What operation would you like to perform?
     1) add
     2) subtract
@@ -66,20 +66,20 @@ loop do #main loop
     4) divide
   MSG
   prompt(operator_prompt)
-  
+
   operator = ''
   loop do
-  operator = Kernel.gets().chomp()
-  
+    operator = Kernel.gets().chomp()
+
     if %w(1 2 3 4).include?(operator)
       break
     else
       prompt("Must choose 1, 2, 3 or 4")
     end
   end
-  
+
   prompt("#{operation_to_message(operator)} the two numbers")
-  
+
   result = case operator
            when '1'
              number1.to_i() + number2.to_i()
@@ -89,10 +89,10 @@ loop do #main loop
              number1.to_i() * number2.to_i()
            when '4'
              number1.to_f() / number2.to_f()
-  end 
-  
+           end
+
   prompt("The result is #{result}")
-  
+
   prompt("Do you want to perform another calculation? (Y to calulate again)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
