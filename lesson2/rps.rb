85694f4b3ec727515c8a156b30d00afd491fb772
Shawn Bartke <cfsilversnake@gmail.com>
2022-07-05 17:34:58 +0000

    rps complete

--- lesson2/rps.rb
+++ lesson2/rps.rb
@@ -4,16 +4,42 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-choice = ''
-loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp()
+def display_results(player, computer)
   
-  if VALID_CHOICES.include?(choice)
-    break
+  if (player == 'rock' && computer == 'scissors') ||
+      (player == 'paper' && computer == 'rock') ||
+      (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won!")
   else
-    prompt("That's not a valid choice.")
+    prompt("It's a tie!")
   end
 end
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+    
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+  
+  computer_choice = VALID_CHOICES.sample
+  
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+  
+  display_results(choice, computer_choice)
+  
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
+end
 
-computer_choice = VALID_CHOICES.sample
+prompt("Thank you for playing. Good Bye!")
