33fbd33308dfaec5b0ae70f55ace66c373cc43ec
Shawn Bartke <cfsilversnake@gmail.com>
2022-07-05 16:10:12 +0000

    rock_paper_scissor start

---
+++ lesson2/rps.rb
@@ -0,0 +1,19 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+choice = ''
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+  choice = Kernel.gets().chomp()
+  
+  if VALID_CHOICES.include?(choice)
+    break
+  else
+    prompt("That's not a valid choice.")
+  end
+end
+
+computer_choice = VALID_CHOICES.sample
